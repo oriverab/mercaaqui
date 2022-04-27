2f5bbd99cd2ccb6ac524d9e0d3821b03211c371c
unknown <SENA@MOSAPRCBAFSD154.FormacionSena.RED>
2022-04-27 11:48:06 -0500

    tablaactualmejorada

--- resources/views/layouts/estructura.blade.php
+++ resources/views/layouts/estructura.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'MercaAqui') }}</title>
 
     <!-- Scripts -->
-    
+  
     <link rel="icon" 
       type="image/png" 
       href="https://cdn-icons-png.flaticon.com/512/3082/3082031.png">
@@ -19,46 +19,9 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link href="/resources/css/app.css" rel="stylesheet">
-    
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<!-- Bootstrap DataTable -->
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css">
-<!-- Bootstrap ExportDataTable -->
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.5.0/css/buttons.bootstrap.min.css">
-<!-- Font icons -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<!-- jQuery -->
-<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<!-- DataTables JS -->
-<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>
-<!--  ExportDataTable JS -->
-<script src="https://cdn.datatables.net/buttons/1.5.0/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.0/js/buttons.bootstrap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.0/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.0/js/buttons.print.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.5.0/js/buttons.colVis.min.js"></script>
-<!-- Bootstrap JS -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-<script type="text/javascript">
-$(document).ready(function() {
-    var table = $('#ExportDataTable').DataTable( {
-        lengthChange: false,
-        //buttons: [ 'copy', 'excel', 'pdf', 'print', 'colvis' ]
-		buttons: [ 'copy', 'excel', 'pdf', 'print']
-    } );
-    table.buttons().container()
-        .appendTo( '#ExportDataTable_wrapper .col-sm-6:eq(0)' );
-} );
-</script>
-    
     <title></title>
   </head>
   <body>
--- resources/views/vendedor/index.blade.php
+++ resources/views/vendedor/index.blade.php
@@ -18,7 +18,40 @@
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<!-- Bootstrap DataTable -->
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css">
+<!-- Bootstrap ExportDataTable -->
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.5.0/css/buttons.bootstrap.min.css">
+<!-- Font icons -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- jQuery -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<!-- DataTables JS -->
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>
+<!--  ExportDataTable JS -->
+<script src="https://cdn.datatables.net/buttons/1.5.0/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.0/js/buttons.bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.0/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.0/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.0/js/buttons.colVis.min.js"></script>
+<!-- Bootstrap JS -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+    var table = $('#ExportDataTable').DataTable( {
+        lengthChange: false,
+        //buttons: [ 'copy', 'excel', 'pdf', 'print', 'colvis' ]
+		buttons: [ 'copy', 'excel', 'pdf', 'print']
+    } );
+    table.buttons().container()
+        .appendTo( '#ExportDataTable_wrapper .col-sm-6:eq(0)' );
+} );
+</script>
 
 </head>
 
